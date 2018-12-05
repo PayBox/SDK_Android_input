0b05e280bd475a6f75adcc846b225c687f02f0d4
arman <am@paybox.money>
2018-12-05 16:27:17 +0600

    parcelable added

--- payboxsdk/src/main/java/money/paybox/payboxsdk/Model/Card.java
+++ payboxsdk/src/main/java/money/paybox/payboxsdk/Model/Card.java
@@ -1,14 +1,12 @@
 package money.paybox.payboxsdk.Model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
 import java.util.Date;
-
 import money.paybox.payboxsdk.Utils.ParseUtils;
 
-/**
- * Created by arman on 16.11.17.
- */
 
-public class Card  {
+public class Card implements Parcelable {
     private String status;
     private String merchantId;
     private String cardId;
@@ -53,4 +51,42 @@ public class Card  {
         return cardhash;
     }
 
+
+    protected Card(Parcel in) {
+        status = in.readString();
+        merchantId = in.readString();
+        cardId = in.readString();
+        recurringProfile = in.readString();
+        cardhash = in.readString();
+        long tmpDate = in.readLong();
+        date = tmpDate != -1 ? new Date(tmpDate) : null;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(status);
+        dest.writeString(merchantId);
+        dest.writeString(cardId);
+        dest.writeString(recurringProfile);
+        dest.writeString(cardhash);
+        dest.writeLong(date != null ? date.getTime() : -1L);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<Card> CREATOR = new Parcelable.Creator<Card>() {
+        @Override
+        public Card createFromParcel(Parcel in) {
+            return new Card(in);
+        }
+
+        @Override
+        public Card[] newArray(int size) {
+            return new Card[size];
+        }
+    };
 }
